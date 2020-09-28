9d8348aa3d11d2824c86de7e0cd3392ba2e1c553
jPlan3t <71607723+jPlan3t@users.noreply.github.com>
2020-09-28 18:17:56 -0400

    Add files via upload

---
+++ getObstructionFactor_system.print.lua
@@ -0,0 +1,10 @@
+function round(num, numDecimalPlaces)
+    local mult = 10 ^ (numDecimalPlaces or 0)
+    if numDecimalPlaces ~= nil then
+        return math.floor(num * mult + 0.5) / mult
+    else
+        return math.floor((num * mult + 0.5) / mult)
+    end
+end
+
+system.print(round(slot1.getObstructionFactor(), 4) * 100)
